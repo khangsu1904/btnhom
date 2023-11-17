9d9ce94ab4e2ec16bc7fe4a4e6382097b93d7848
khangsu1904 <150413129+khangsu1904@users.noreply.github.com>
2023-11-17 21:57:21 +0700

    Exercise61.py

--- Exercise61.py
+++ Exercise61.py
@@ -1,1 +1,14 @@
-print('sadsa')
+sum=0
+n=0
+while True:
+    num=int(input())
+    if (not num==0):
+        sum=sum+num
+        n+=1
+        avr=sum/n
+    else:
+        break
+print(avr)
+    
+
+    
