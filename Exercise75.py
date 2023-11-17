4a3bc7840ba832cc9bee07d50caa95c70258c828
khangsu1904 <150413129+khangsu1904@users.noreply.github.com>
2023-11-17 22:25:38 +0700

    Exercise75.py

--- Exercise75.py
+++ Exercise75.py
@@ -1,11 +1,10 @@
-def find_gcd(n, m):
-    d = min(n, m)
-    while d > 0:
-        if n % d == 0 and m % d == 0:
-            break
-        d = 1
-    return d 
-num1 = int(input('Enter the first positive integer: '))
-num2 = int(input('Enter the second positive integer: '))
-gcd = find_gcd(num1, num2)
-print(f'The greatest common divisor of {num1} and {num2} is: {gcd}')
+m=int(input())
+n=int(input())
+d = min(n, m)
+while True:
+    if not(n%d == 0 and m%d == 0):
+        d-=1
+    else:
+        break
+print(f'The greatest common divisor of {m} and {n} is: {d}')   
+    
