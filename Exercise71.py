b965fe37afc33c856fafa60536ea9e41e93c2d02
ThaoMy245 <150454505+ThaoMy245@users.noreply.github.com>
2023-11-17 23:01:18 +0700

    Exercise71.py

--- Exercise71.py
+++ Exercise71.py
@@ -1,12 +1,5 @@
-def newtons_method_sqrt(x):
-    guess = x/2.0
-    threshold = 1e-12
-    while abs( guess * guess - x ) > threshold:
-        guess = ( guess + x / guess ) / 2.0
-    return guess
-user_number = float(input('Enter a number to find the square root: '))
-if user_number < 0:
-    print('Please enter a non-negative number')
-else:
-    result = newtons_method_sqrt(user_number)
-    print (f'The square root of {user_number} is approximately: {result}' )   
+x = float(input("Enter a number: "))
+guess = x / 2
+while abs(guess * guess - x) > 1e-12:
+    guess = (guess + x / guess) / 2
+print(f"The square root of {x} is approximately {guess}")
