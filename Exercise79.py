69ec79a625f46babc385567a806bb273a267618a
khangsu1904 <150413129+khangsu1904@users.noreply.github.com>
2023-11-17 11:09:27 +0700

    Update Exercise79.py

--- Exercise79.py
+++ Exercise79.py
@@ -1,19 +1,17 @@
+import random
 
+updates=0
 
-num=int(input())
-max_num = randrange(1, num + 1)
-print(max_num)
-
-updates = 0
-
-for i in range(1, num):
-    current = randrange(1, num + 1)
-    if current > max_num:
-        max_num = current
-        updates += 1
-        print('{}   <= Update'. format(current))
+num=100
+max=random.randrange(1,num+1)
+print(max)
+for i in range(1,num):
+    current=random.randrange(1,num+1)
+    if current>max:
+        updates=updates+1
+        max=current
+        print(f'{max}  <== Update')
     else:
         print(current)
-
-print('The maximum number found was', max_num)
-print('The maximum number was updated {} times'. format(updates))
+print(f'The maximum value found was {max}')
+print(f'The maximum value updated {updates} times')
