6bd8340adc75fc8460939f30c14aca5a226cbaea
khangsu1904 <150413129+khangsu1904@users.noreply.github.com>
2023-11-17 22:59:49 +0700

    Exercise72.py

--- Exercise72.py
+++ Exercise72.py
@@ -1,9 +1,9 @@
 string=str(input())
-Sn=''
+a=''
 n=len(string)
 for i in range(n-1,-1,-1):
-    Sn=Sn+string[i]
-if(Sn==string):
+    a=a+string[i]
+if(a==string):
     print(f'{string} is a palindrome.')
 else:
     print(f'{string} is not a palindrome.')
